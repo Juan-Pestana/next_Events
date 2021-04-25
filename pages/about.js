ba9448dd94d9ed7024cc61501a558ea6de5cd79a
Juan Pestana <juan.pestana82@gmail.com>
2021-04-25 20:48:42 +0200

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>Página de About</h1>
+            <p>Esta es una página donde encontrar eventos y djs</p>
+            <p>Versión 1.0.0</p>
+        </div>
+    )
+}
